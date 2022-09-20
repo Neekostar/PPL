b2afcfbb2184fe35e8012325a151a7b80418cb3b
Neekostar <nekitcus@mail.ru>
2022-09-20 21:22:06 +0300

    initial commit

--- LAB2/EX1.py
+++ LAB2/EX1.py
@@ -1,8 +1,10 @@
 N = int(input('Введите количество контактов в телефонной книге:\n'))
-dictionary = {}
+phone_book = {}
+
 for i in range(N):
     key = int(input(f'Введите номер телефона {i + 1}-го контакта:\n'))
     value = input('Введите пользователя:\n')
-    dictionary[key] = value
-result = list(dictionary.values())
+    phone_book[key] = value
+
+result = list(phone_book.values())
 print(result)
